ef5bdb469ff8f84431b84f41890b00a92b1a6dc6
Konstantin Büschel <konstantin.bueschel@gmail.com>
2019-05-27 15:07:24 +0200

    fix: remove element node check, improve code readability

--- index.js
+++ index.js
@@ -50,253 +50,239 @@ function tich() {
           status();
         }
       }
-    } else {
-      // find the config name specified
-      cfg.configs.forEach(function(config) {
-        if (config.name === name) {
-          console.log('\nFound a config for ' + chalk.cyan(config.name) + '\n');
-
-          if (fs.existsSync('./app/config.json')) {
-            var alloyCfg = JSON.parse(
-              fs.readFileSync('./app/config.json', 'utf-8'),
-            );
-
-            if (alloyCfg.global.theme) {
-              var original = alloyCfg.global.theme;
-
-              alloyCfg.global.theme = name;
-
-              fs.writeFile(
-                './app/config.json',
-                JSON.stringify(alloyCfg, null, 2),
-                function(err) {
-                  if (err) return console.log(err);
-                  console.log(
-                    '\nChanging theme value in config.json from ' +
-                      chalk.cyan(original) +
-                      ' to ' +
-                      chalk.cyan(name),
-                  );
-                },
-              );
-            }
-          }
-
-          // settings without properties and raw
-          for (var setting in config.settings) {
-            if (!config.settings.hasOwnProperty(setting)) {
-              continue;
-            }
-
-            if (setting !== 'properties' && setting !== 'raw') {
-              var replaceWith = config.settings[setting];
-
-              if (typeof replaceWith === 'string') {
-                var now = new Date(),
-                  matchResult;
-
-                replaceWith
-                  .replace('$DATE$', now.toLocaleDateString())
-                  .replace('$TIME$', now.toLocaleTimeString())
-                  .replace('$DATETIME$', now.toLocaleString())
-                  .replace('$TIME_EPOCH$', now.getTime().toString());
-
-                while ((matchResult = regex.exec(replaceWith))) {
-                  replaceWith = replaceWith.replace(
-                    matchResult[0],
-                    tiapp[matchResult[1]],
-                  );
-                }
-
-                while ((matchResult = regexProperty.exec(replaceWith))) {
-                  replaceWith = replaceWith.replace(
-                    matchResult[0],
-                    tiapp.getProperty(matchResult[1]),
-                  );
-                }
-              }
-
-              tiapp[setting] = replaceWith;
-
-              console.log(
-                'Changing ' +
-                  chalk.cyan(setting) +
-                  ' to ' +
-                  chalk.yellow(replaceWith),
-              );
-            }
-          }
-
-          // settings.properties
-          if (config.settings.properties) {
-            for (var property in config.settings.properties) {
-              if (!config.settings.properties.hasOwnProperty(property)) {
-                continue;
-              }
-
-              var replaceWith = config.settings.properties[property];
-
-              if (typeof replaceWith === 'string') {
-                var now = new Date(),
-                  matchResult;
-
-                replaceWith
-                  .replace('$DATE$', now.toLocaleDateString())
-                  .replace('$TIME$', now.toLocaleTimeString())
-                  .replace('$DATETIME$', now.toLocaleString())
-                  .replace('$TIME_EPOCH$', now.getTime().toString());
-
-                while ((matchResult = regex.exec(replaceWith))) {
-                  replaceWith = replaceWith.replace(
-                    matchResult[0],
-                    tiapp[matchResult[1]],
-                  );
-                }
-
-                while ((matchResult = regexProperty.exec(replaceWith))) {
-                  replaceWith = replaceWith.replace(
-                    matchResult[0],
-                    tiapp.getProperty(matchResult[1]),
-                  );
-                }
-              }
-
-              var propertyType = 'string';
-
-              _.isInteger(replaceWith) && (propertyType = 'int');
-
-              _.isNumber(replaceWith) &&
-                !_.isInteger(replaceWith) &&
-                (propertyType = 'double');
-
-              _.isBoolean(replaceWith) && (propertyType = 'bool');
-
-              tiapp.setProperty(property, replaceWith, propertyType);
-
-              console.log(
-                'Changing App property ' +
-                  chalk.cyan(property) +
-                  ' to ' +
-                  chalk.yellow(replaceWith),
-              );
-            }
-          }
-
-          // settings.raw
-          if (config.settings.raw) {
-            var doc = tiapp.doc;
-            var select = xpath.useNamespaces({
-              ti: 'http://ti.appcelerator.org',
-              android: 'http://schemas.android.com/apk/res/android',
-            });
-
-            for (var path in config.settings.raw) {
-              if (!config.settings.raw.hasOwnProperty(path)) {
-                continue;
-              }
-
-              var node = select(path, doc, true);
-
-              if (!node) {
-                console.log(
-                  chalk.yellow('Could not find ' + path + ', skipping'),
-                );
-                continue;
-              }
-
-              if (node.nodeType === 1) {
-                console.log(chalk.yellow('ELEMENT_NODE found, skipping'));
-                continue;
-              }
-
-              var replaceWith = config.settings.raw[path];
-
-              if (typeof replaceWith === 'string') {
-                var now = new Date();
-
-                replaceWith
-                  .replace('$DATE$', now.toLocaleDateString())
-                  .replace('$TIME$', now.toLocaleTimeString())
-                  .replace('$DATETIME$', now.toLocaleString())
-                  .replace('$TIME_EPOCH$', now.getTime().toString());
-              }
-
-              var matchResult,
-                changed = false;
-
-              while ((matchResult = regex.exec(replaceWith))) {
-                replaceWith = replaceWith.replace(
-                  matchResult[0],
-                  tiapp[matchResult[1]],
-                );
-              }
-
-              while ((matchResult = regexProperty.exec(replaceWith))) {
-                replaceWith = replaceWith.replace(
-                  matchResult[0],
-                  tiapp.getProperty(matchResult[1]),
-                );
-              }
-
-              if (_.hasIn(node, 'value')) {
-                node.value = replaceWith;
-                changed = true;
-              } else if (_.hasIn(node, 'data')) {
-                node.data = replaceWith;
-                changed = true;
-              } else if (_.hasIn(node, 'firstChild')) {
-                node.firstChild.data = replaceWith;
-                changed = true;
-              }
-
-              changed &&
-                console.log(
-                  'Changing Raw property ' +
-                    chalk.cyan(path) +
-                    ' to ' +
-                    chalk.yellow(replaceWith),
-                );
-            }
-          }
-
-          if (
-            fs.existsSync(
-              './app/themes/' + name + '/assets/iphone/DefaultIcon.png',
-            )
-          ) {
-            // if it exists in the themes folder, in a platform subfolder
-            console.log(
-              chalk.blue(
-                "Found a DefaultIcon.png in the theme's assets/iphone folder\n",
-              ),
-            );
-
-            copyFile(
-              './app/themes/' + name + '/assets/iphone/DefaultIcon.png',
-              './DefaultIcon.png',
-            );
-          } else if (
-            fs.existsSync('./app/themes/' + name + '/DefaultIcon.png')
-          ) {
-            // if it exists in the top level theme folder
-            console.log(
-              chalk.blue('Found a DefaultIcon.png in the theme folder\n'),
-            );
-
-            copyFile(
-              './app/themes/' + name + '/' + '/DefaultIcon.png',
-              './DefaultIcon.png',
-            );
-          }
-
-          console.log(chalk.green('\n' + outfilename + ' updated\n'));
-
-          tiapp.write(outfilename);
-        }
-      });
-
-      // console.log(chalk.red('\nCouldn\'t find a config called: ' + name + '\n'));
+      return;
     }
+
+    // find the config name specified
+    cfg.configs.forEach(function(config) {
+      if (config.name !== name) return;
+
+      console.log('\nFound a config for ' + chalk.cyan(config.name) + '\n');
+
+      if (fs.existsSync('./app/config.json')) {
+        var alloyCfg = JSON.parse(
+          fs.readFileSync('./app/config.json', 'utf-8'),
+        );
+
+        if (alloyCfg.global.theme) {
+          var original = alloyCfg.global.theme;
+
+          alloyCfg.global.theme = name;
+
+          fs.writeFile(
+            './app/config.json',
+            JSON.stringify(alloyCfg, null, 2),
+            function(err) {
+              if (err) return console.log(err);
+              console.log(
+                '\nChanging theme value in config.json from ' +
+                  chalk.cyan(original) +
+                  ' to ' +
+                  chalk.cyan(name),
+              );
+            },
+          );
+        }
+      }
+
+      // settings without properties and raw
+      for (var setting in config.settings) {
+        if (!config.settings.hasOwnProperty(setting)) continue;
+
+        if (setting !== 'properties' && setting !== 'raw') {
+          var replaceWith = config.settings[setting];
+
+          if (typeof replaceWith === 'string') {
+            var now = new Date(),
+              matchResult;
+
+            replaceWith
+              .replace('$DATE$', now.toLocaleDateString())
+              .replace('$TIME$', now.toLocaleTimeString())
+              .replace('$DATETIME$', now.toLocaleString())
+              .replace('$TIME_EPOCH$', now.getTime().toString());
+
+            while ((matchResult = regex.exec(replaceWith))) {
+              replaceWith = replaceWith.replace(
+                matchResult[0],
+                tiapp[matchResult[1]],
+              );
+            }
+
+            while ((matchResult = regexProperty.exec(replaceWith))) {
+              replaceWith = replaceWith.replace(
+                matchResult[0],
+                tiapp.getProperty(matchResult[1]),
+              );
+            }
+          }
+
+          tiapp[setting] = replaceWith;
+
+          console.log(
+            'Changing ' +
+              chalk.cyan(setting) +
+              ' to ' +
+              chalk.yellow(replaceWith),
+          );
+        }
+      }
+
+      // settings.properties
+      if (config.settings.properties) {
+        for (var property in config.settings.properties) {
+          if (!config.settings.properties.hasOwnProperty(property)) {
+            continue;
+          }
+
+          var replaceWith = config.settings.properties[property];
+
+          if (typeof replaceWith === 'string') {
+            var now = new Date(),
+              matchResult;
+
+            replaceWith
+              .replace('$DATE$', now.toLocaleDateString())
+              .replace('$TIME$', now.toLocaleTimeString())
+              .replace('$DATETIME$', now.toLocaleString())
+              .replace('$TIME_EPOCH$', now.getTime().toString());
+
+            while ((matchResult = regex.exec(replaceWith))) {
+              replaceWith = replaceWith.replace(
+                matchResult[0],
+                tiapp[matchResult[1]],
+              );
+            }
+
+            while ((matchResult = regexProperty.exec(replaceWith))) {
+              replaceWith = replaceWith.replace(
+                matchResult[0],
+                tiapp.getProperty(matchResult[1]),
+              );
+            }
+          }
+
+          var propertyType = 'string';
+
+          _.isInteger(replaceWith) && (propertyType = 'int');
+
+          _.isNumber(replaceWith) &&
+            !_.isInteger(replaceWith) &&
+            (propertyType = 'double');
+
+          _.isBoolean(replaceWith) && (propertyType = 'bool');
+
+          tiapp.setProperty(property, replaceWith, propertyType);
+
+          console.log(
+            'Changing App property ' +
+              chalk.cyan(property) +
+              ' to ' +
+              chalk.yellow(replaceWith),
+          );
+        }
+      }
+
+      // settings.raw
+      if (config.settings.raw) {
+        var doc = tiapp.doc;
+        var selectXpath = xpath.useNamespaces({
+          ti: 'http://ti.appcelerator.org',
+          android: 'http://schemas.android.com/apk/res/android',
+        });
+
+        for (var path in config.settings.raw) {
+          if (!config.settings.raw.hasOwnProperty(path)) {
+            continue;
+          }
+
+          var node = selectXpath(path, doc, true);
+
+          if (!node) {
+            console.log(chalk.yellow('Could not find ' + path + ', skipping'));
+            continue;
+          }
+
+          var replaceWith = config.settings.raw[path];
+
+          if (typeof replaceWith === 'string') {
+            var now = new Date();
+
+            replaceWith
+              .replace('$DATE$', now.toLocaleDateString())
+              .replace('$TIME$', now.toLocaleTimeString())
+              .replace('$DATETIME$', now.toLocaleString())
+              .replace('$TIME_EPOCH$', now.getTime().toString());
+          }
+
+          var matchResult,
+            changed = false;
+
+          while ((matchResult = regex.exec(replaceWith))) {
+            replaceWith = replaceWith.replace(
+              matchResult[0],
+              tiapp[matchResult[1]],
+            );
+          }
+
+          while ((matchResult = regexProperty.exec(replaceWith))) {
+            replaceWith = replaceWith.replace(
+              matchResult[0],
+              tiapp.getProperty(matchResult[1]),
+            );
+          }
+
+          if (_.hasIn(node, 'value')) {
+            node.value = replaceWith;
+            changed = true;
+          } else if (_.hasIn(node, 'data')) {
+            node.data = replaceWith;
+            changed = true;
+          } else if (_.hasIn(node, 'firstChild')) {
+            node.firstChild.data = replaceWith;
+            changed = true;
+          }
+
+          changed &&
+            console.log(
+              'Changing Raw property ' +
+                chalk.cyan(path) +
+                ' to ' +
+                chalk.yellow(replaceWith),
+            );
+        }
+      }
+
+      if (
+        fs.existsSync('./app/themes/' + name + '/assets/iphone/DefaultIcon.png')
+      ) {
+        // if it exists in the themes folder, in a platform subfolder
+        console.log(
+          chalk.blue(
+            "Found a DefaultIcon.png in the theme's assets/iphone folder\n",
+          ),
+        );
+
+        copyFile(
+          './app/themes/' + name + '/assets/iphone/DefaultIcon.png',
+          './DefaultIcon.png',
+        );
+      } else if (fs.existsSync('./app/themes/' + name + '/DefaultIcon.png')) {
+        // if it exists in the top level theme folder
+        console.log(
+          chalk.blue('Found a DefaultIcon.png in the theme folder\n'),
+        );
+
+        copyFile(
+          './app/themes/' + name + '/' + '/DefaultIcon.png',
+          './DefaultIcon.png',
+        );
+      }
+
+      console.log(chalk.green('\n' + outfilename + ' updated\n'));
+
+      tiapp.write(outfilename);
+    });
   }
 
   // setup CLI
